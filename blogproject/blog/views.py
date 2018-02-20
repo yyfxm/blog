36598ea674be90595f03b7e409ef97e4f5072b29
yyfxm <yangyafangxm@163.com>
2018-02-20 23:48:57 +0800

    基本完成

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Category,Tag
 import markdown
+from comments.forms import CommentForm
 
 # Create your views here.
 
@@ -15,7 +16,13 @@ def detail(request,pk):
                                     'markdown.extensions.codehilite',
                                     'markdown.extensions.toc',
                                  ])
-    return render(request,'blog/detail.html',context={'post':post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post':post,
+               'form':form,
+               'comment_list':comment_list
+              }
+    return render(request,'blog/detail.html',context=context)
 def archives(request,year,month):
     post_list = Post.objects.filter(create_time__year=year,
                                     create_time__month=month
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import url,include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')),
+    url(r'',include('comments.urls')),
 
 ]
