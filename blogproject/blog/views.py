ef01a11d5e92034021044e82bb1143e563b80718
yyfxm <yangyafangxm@163.com>
2018-02-20 12:01:27 +0800

    高亮设置完成

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Category,Tag
-
+import markdown
 
 # Create your views here.
 
@@ -9,4 +9,10 @@ def index(request):
     return render(request,'blog/index.html',context={'post_list':post_list})
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
+    post.body = markdown.markdown(post.body,
+                                 extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                 ])
     return render(request,'blog/detail.html',context={'post':post})
