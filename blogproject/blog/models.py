8593c6bd534fb344932e0505171f532c980d50ff
yyfxm <yangyafangxm@163.com>
2018-02-20 11:15:24 +0800

    细节页URL配置完成

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -22,3 +22,5 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
